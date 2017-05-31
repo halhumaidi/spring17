bffc3a5c3a9a5307c34260ed35f4c393b724ba13
halhumaidi <haya.b.alhumaidi@gmail.com>
2017-05-31 21:44:33 +0300

    completed crud views for admin

--- main/urls.py
+++ main/urls.py
@@ -10,5 +10,11 @@ urlpatterns = [
     url(r'^editCoffee/(?P<coffee_id>[0-9])$',views.editCoffee, name = "editCoffee"),
     url(r'^createBean/$',views.createBean, name = "createBean"),
     url(r'^editBean/(?P<bean_id>[0-9])$',views.editBean, name = "editBean"),
+    url(r'^createRoast/$',views.createRoast, name = "createRoast"),
+    url(r'^editRoast/(?P<roast_id>[0-9])$',views.editRoast, name = "editRoast"),
+    url(r'^createSyrup/$',views.createSyrup, name = "createSyrup"),
+    url(r'^editSyrup/(?P<syrup_id>[0-9])$',views.editSyrup, name = "editSyrup"),
+    url(r'^createPowder/$',views.createPowder, name = "createPowder"),
+    url(r'^editPowder/(?P<powder_id>[0-9])$',views.editPowder, name = "editPowder"),
 
 ]
--- main/views.py
+++ main/views.py
@@ -87,3 +87,114 @@ def editBean(request, bean_id):
 def deleteBean(request, bean_id):
     Bean.objects.get(id=bean_id).delete()
     return redirect("main:home")
+
+def createRoast(request):
+    context={}
+    if request.method == "POST":
+        form = RoastForm(request.POST)
+        context["form"]=form
+        if form.is_valid():
+            form.save()
+            return redirect("main:home")
+        else:
+            return render(request, "main/createRoast.html", context)
+    else:
+        form = RoastForm()
+        context["form"]=form
+        return render(request, "main/createRoast.html", context)
+
+def editRoast(request, roast_id):
+    context = {}
+    roast = Roast.objects.get(id=roast_id)
+    context["roast"]=roast
+
+    if request.method == "POST":
+        form = RoastForm(request.POST, instance=roast)
+        context["form"]=form
+        if form.is_valid():
+            form.save()
+            return redirect("main:home")
+        else:
+            return render(request, "main/editRoast.html", context)
+    else:
+        form = RoastForm(instance=roast)
+        context["form"]=form
+        return render(request, "main/editRoast.html", context)
+
+def deleteRoast(request, roast_id):
+    Roast.objects.get(id=roast_id).delete()
+    return redirect("main:home")
+
+def createSyrup(request):
+    context={}
+    if request.method == "POST":
+        form = SyrupForm(request.POST)
+        context["form"]=form
+        if form.is_valid():
+            form.save()
+            return redirect("main:home")
+        else:
+            return render(request, "main/createSyrup.html", context)
+    else:
+        form = SyrupForm()
+        context["form"]=form
+        return render(request, "main/createSyrup.html", context)
+
+def editSyrup(request, syrup_id):
+    context = {}
+    syrup = Syrup.objects.get(id=syrup_id)
+    context["syrup"]=syrup
+
+    if request.method == "POST":
+        form = SyrupForm(request.POST, instance=syrup)
+        context["form"]=form
+        if form.is_valid():
+            form.save()
+            return redirect("main:home")
+        else:
+            return render(request, "main/editSyrup.html", context)
+    else:
+        form = SyrupForm(instance=syrup)
+        context["form"]=form
+        return render(request, "main/editSyrup.html", context)
+
+def deleteSyrup(request, syrup_id):
+    Syrup.objects.get(id=syrup_id).delete()
+    return redirect("main:home")
+
+def createPowder(request):
+    context={}
+    if request.method == "POST":
+        form = PowderForm(request.POST)
+        context["form"]=form
+        if form.is_valid():
+            form.save()
+            return redirect("main:home")
+        else:
+            return render(request, "main/createPowder.html", context)
+    else:
+        form = PowderForm()
+        context["form"]=form
+        return render(request, "main/createPowder.html", context)
+
+def editPowder(request, powder_id):
+    context = {}
+    powder = Powder.objects.get(id=powder_id)
+    context["powder"]=powder
+
+    if request.method == "POST":
+        form = PowderForm(request.POST, instance=powder)
+        context["form"]=form
+        if form.is_valid():
+            form.save()
+            return redirect("main:home")
+        else:
+            return render(request, "main/editPowder.html", context)
+    else:
+        form = PowderForm(instance=powder)
+        context["form"]=form
+        return render(request, "main/editPowder.html", context)
+
+def deletePowder(request, powder_id):
+    Powder.objects.get(id=powder_id).delete()
+    return redirect("main:home")
