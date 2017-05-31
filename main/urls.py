edd591727f20a4453110f43ea880d8c23a372ad8
halhumaidi <haya.b.alhumaidi@gmail.com>
2017-05-31 23:16:20 +0300

    completed user view stuff for admin

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
 
     url(r'^$',views.main),
     url(r'^home/$',views.home, name = "home"),
+    url(r'^user_list/$',views.user_list, name = "user_list"),
+    url(r'^user_coffees/(?P<user_id>[0-9])$',views.user_coffees, name = "user_coffees"),
     url(r'^createCoffee/$',views.createCoffee, name = "createCoffee"),
     url(r'^editCoffee/(?P<coffee_id>[0-9])$',views.editCoffee, name = "editCoffee"),
     url(r'^createBean/$',views.createBean, name = "createBean"),
